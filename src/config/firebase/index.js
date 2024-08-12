76f4f8ddb72ea63136facab51f944e7ed3667597
Joshtri <stuffofyos1516@gmail.com>
2024-08-12 12:06:58 +0800

    delete scrt ky

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -1,12 +1,13 @@
 import firebase from "firebase/compat/app";
 
+
 firebase.initializeApp({
-    apiKey: "AIzaSyDPa11qi0HJJyYBzhRc6sxPMiWbr-XKPuI",
-    authDomain: "crudnativereact.firebaseapp.com",
-    projectId: "crudnativereact",
-    storageBucket: "crudnativereact.appspot.com",
-    messagingSenderId: "861007484071",
-    appId: "1:861007484071:web:65d310b0996d75fed8f782"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const FIREBASE = firebase;
